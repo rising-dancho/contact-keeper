58e042d66634feee538d7ae4a7373244bf5963e7
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-08-02 03:42:32 +0800

    feat: creating routes

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const baseUrl = '/api/v1';
 
 app.get('/', (req, res) => res.send({ msg: 'App: Contact Keeper' }));
+
+// Define routes
+app.use(`${baseUrl}/users`, require('./routes/users'));
+app.use(`${baseUrl}/auth`, require('./routes/auth'));
+app.use(`${baseUrl}/contacts`, require('./routes/contacts'));
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
