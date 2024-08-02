56ab5f0fc0adc622ea7780b0f726e68fc3e58326
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-08-02 19:52:19 +0800

    fix: adding a vercel.json file for deployment

--- config/db.js
+++ config/db.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-  mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB Connected'))
-    .catch((err) => {
-      console.error(err.message);
-      process.exit(1);
-    });
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
