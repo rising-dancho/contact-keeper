ada9a988659089b6e78472c5e4220c5b0bfd5f0c
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-08-02 19:21:50 +0800

    feat: created a config folder for db connection

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 const baseUrl = '/api/v1';
 
