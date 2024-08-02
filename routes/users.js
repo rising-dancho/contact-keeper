929403de658004906efe7c9bd90cbb84e73e7f12
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-08-03 01:46:34 +0800

    feat: added a route level validation for the form inputs

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const { check, validationResult } = require('express-validator');
 
 // @route   POST api/v1/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => res.send('Registers a user'));
+router.post(
+  '/',
+  [
+    check('name', 'Please add a name').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+
+    res.send('passed');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 const PORT = process.env.PORT || 5000;
 const baseUrl = '/api/v1';
 
@@ -17,3 +20,5 @@ app.use(`${baseUrl}/auth`, require('./routes/auth'));
 app.use(`${baseUrl}/contacts`, require('./routes/contacts'));
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
+// express-validator, what is it for? https://chatgpt.com/share/ee1118bd-4d17-4232-bea4-db50d85467e6
