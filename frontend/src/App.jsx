5392548755ca60a291ddcde650c6f92c9eb236ca
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-08-07 06:00:40 +0800

    feat: initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+
 function App() {
-  return <h1>Happy Coding 🚀</h1>;
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
