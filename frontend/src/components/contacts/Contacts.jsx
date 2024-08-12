1b64b2d422d926e58d7fdd25b87ed2be4d6a1e11
Jose Alejo Perez Jr <43742265+rising-dancho@users.noreply.github.com>
2024-08-13 00:01:04 +0800

    feat: added transition effects

--- frontend/src/components/contacts/Contacts.jsx
+++ frontend/src/components/contacts/Contacts.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import ContactContext from '../context/contact/contactContext';
 import ContactItem from './ContactItem';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
@@ -11,13 +12,19 @@ const Contacts = () => {
   }
   return (
     <>
-      {filtered !== null
-        ? filtered.map((contact) => (
-            <ContactItem key={contact.id} contact={contact} />
-          ))
-        : contacts.map((contact) => {
-            return <ContactItem key={contact.id} contact={contact} />;
-          })}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames="item">
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames="item">
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </>
   );
 };
